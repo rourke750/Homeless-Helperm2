3326093d91d9ebde23d14015a56faabe9b06d3dd
Dimitrije Taylor <dimitd5@gmail.com>
2018-01-23 12:30:58 -0500

    Added my name
    and student ID

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -70,12 +70,12 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Dimitrije");
-                  toPrint += p4.toString("dtaylor78");
+                  toPrint += p4.toString(" dtaylor78");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Rourke Rabinowitz");
                   toPrint += p5.toString("903133839");
-        System.out.println(toPrint);
+        System.out.println(toPrint); 
 
 
     }
